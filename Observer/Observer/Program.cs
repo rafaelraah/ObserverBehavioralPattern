5407c950b19c0a63843277b98b837af17397a1f8
Rafael <rafaelsantos.rsdn@gmail.com>
2021-11-19 00:51:38 -0300

    First Commit

---
+++ Observer/Observer/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesignPatterns;
+using System;
+
+namespace Observer
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ExecucaoObserver.Executar();
+        }
+    }
+}
